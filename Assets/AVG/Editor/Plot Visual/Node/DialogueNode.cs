e8c109af8d840e812a696c6309e4b727d5b2fbf4
AvA <Morsiusiurandum@outlook.com>
2022-09-07 16:51:29 +0800

    Visual effects adjustments

--- Assets/AVG/Editor/Plot Visual/Node/DialogueNode.cs
+++ Assets/AVG/Editor/Plot Visual/Node/DialogueNode.cs
@@ -8,9 +8,9 @@ namespace AVG.Editor.Plot_Visual
 {
     internal sealed class DialogueNode : GraphNode<DialogueSection>
     {
-        public DialogueNode()
+        public DialogueNode(DialogueSection section = null)
         {
-            Section = new DialogueSection();
+            Section = section ?? new DialogueSection();
             var visualAsset = EditorGUIUtility.Load("GraphNode.uxml") as VisualTreeAsset;
             VisualElement = CreatVisual(visualAsset);
         }
@@ -18,9 +18,10 @@ namespace AVG.Editor.Plot_Visual
         private protected override VisualElement CreatVisual(VisualTreeAsset uxml)
         {
             var visualElement = base.CreatVisual(uxml);
-
+            Foldout foldout = visualElement.Query<Foldout>("Fold");
+            Button addButton = visualElement.Query<Button>("Add");
+            VisualElement dialogue = visualElement.Query<VisualElement>("Base");
             TextField characterName = visualElement.Query<TextField>("CharacterName");
-
             characterName.value = Section?.characterName;
             characterName.RegisterValueChangedCallback(
                 _ => { Section.characterName = characterName.value; }
@@ -31,10 +32,18 @@ namespace AVG.Editor.Plot_Visual
             dialogueText.RegisterValueChangedCallback(
                 _ => { Section.dialogueText = dialogueText.value; }
             );
+            foldout.Add(dialogue);
+            addButton.clicked += () =>
+            {
+                var element = new VisualElement();
+                element.Add(new TextField("Name"));
+                element.Add(new TextField("Dialogue"));
+                foldout.Add(element);
+            };
             return visualElement;
         }
 
-        public override void NodeVisual()
+        protected override void NodeVisual()
         {
             var inputPort =
                 InstantiatePort(Orientation.Horizontal, Direction.Input, Port.Capacity.Multi, typeof(float));
